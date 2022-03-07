1f1a80dfd6f2673453656289eae0d00bc3fb4286
arifpehlivan <arifpehlivanapap@gmail.com>
2022-03-08 02:07:36 +0300

    add db

--- library/books/books.js
+++ library/books/books.js
@@ -1,5 +1,18 @@
 const express=require("express");
 const app=express();
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB, ()=>{
+    console.log("Db connected");
+});
+
+
+app.get("/", (req,res)=>{
+    res.send("Books");
+});
 
 app.listen(4545, ()=>{
     console.log("Running books service");
