c3f61725efca4b822f68d266864456fcd3c17823
arifpehlivan <arifpehlivanapap@gmail.com>
2022-03-08 18:57:34 +0300

    add post

--- library/customers/customers.js
+++ library/customers/customers.js
@@ -2,6 +2,9 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const dotenv=require("dotenv");
+const bodyParser=require("body-parser");
+
+app.use(bodyParser.json());
 
 dotenv.config();
 
@@ -9,6 +12,24 @@ mongoose.connect(process.env.MONGODB, ()=>{
     console.log("Db connected customer service");
 });
 
+require("./Customer");
+const Customer=mongoose.model("Customer");
+
+
+app.post("/customer", (req,res)=>{
+    const newCustomer={
+        name:req.body.name,
+        age:req.body.age,
+        address:req.body.address
+    }
+    const customer=new Customer(newCustomer);
+
+    customer.save().then(()=>{
+        res.send("Customer created");
+    }).catch((err)=>{
+        console.log(err);
+    });
+})
 
 app.listen("5555", ()=>{
     console.log("Customers service");
